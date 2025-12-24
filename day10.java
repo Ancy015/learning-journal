5e35a620582b2135add2c8a7598e105d1f64b059
Ancy <ancy.cs24@stellamaryscoe.edu.in>
2025-12-24 20:45:35 +0530

    first commit

---
+++ day10.java
@@ -0,0 +1,24 @@
+class ElectricityBill
+{
+    public static void main(String args[])
+    {
+        int units = 250;
+        double bill = 0;
+
+        if (units <= 100)
+        {
+            bill = units * 1.5;
+        }
+        else if (units <= 200)
+        {
+            bill = (100 * 1.5) + (units - 100) * 2.5;
+        }
+        else
+        {
+            bill = (100 * 1.5) + (100 * 2.5) + (units - 200) * 4;
+        }
+
+        System.out.println("Electricity Bill = ₹" + bill);
+    }
+}
+
