3c8c0c28fca4a82945c74cfac1b24f60ea6d7a88
Ancy <ancy.cs24@stellamaryscoe.edu.in>
2025-12-31 21:49:55 +0530

    Add BattingAverage.java

---
+++ BattingAverage.java
@@ -0,0 +1,12 @@
+
+public class BattingAverage {
+    public static void main(String[] args) {
+
+        int totalRuns = 350;
+        int timesOut = 15;
+
+        double battingAverage = (double) totalRuns / timesOut;
+
+        System.out.println("Batting Average = " + battingAverage);
+    }
+}
