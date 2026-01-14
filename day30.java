e58d2d9c9788b2cb970a6218fea970fbdd3cc0fb
Ancy <ancy.cs24@stellamaryscoe.edu.in>
2026-01-14 22:53:17 +0530

    Add day30 PrimeNumberCheck class to determine if a number is prime

---
+++ day30.java
@@ -0,0 +1,31 @@
+public class PrimeNumberCheck {
+
+    
+    public boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+
+        int number = 17;
+
+        
+        PrimeNumberCheck obj1 = new PrimeNumberCheck();
+
+        // Method call using object
+        if (obj1.isPrime(number)) {
+            System.out.println(number + " is a Prime Number");
+        } else {
+            System.out.println(number + " is not a Prime Number");
+        }
+    }
+}
+
